13bf01c82fe7b34a0cf04645ddd2b7bf41e90a3d
Muhammad Burhan Bin Din (Centific Technologies inc) <v-mbindin@microsoft.com>
2024-11-19 23:16:30 +0800

    command with option

--- Program.cs
+++ Program.cs
@@ -12,23 +12,23 @@ namespace SampleSpace
         {
 
 
-            // Create a sub command with a description
-            var subCommand = new Command("number", "SubCommand with arguments");
-
-
-            subCommand.SetHandler(()=>{
-
-                Console.WriteLine("Sub Command");
-
-            });
+            // Create an option with a description
+            var singleOption = new Option<bool>
+            (name: "--single",
+            description: "An option whose argument is parsed as an int.");
 
             // Create a root command with a description
             var rootCommand = new RootCommand
             {
-                Description = "Command with subcommand"
+                Description = "Command with option"
             };
 
-            rootCommand.Add(subCommand);
+            rootCommand.Add(singleOption);
+            rootCommand.SetHandler((singleOptionValue) =>
+            {
+                Console.WriteLine($"--single = {singleOptionValue}");
+            },
+            singleOption);
 
             // Check if no arguments are provided and display help
             if (args.Length == 0)
